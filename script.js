91e68de2a6c7ffddf51de257064901a099110661
BOGGALA YASWANTH REDDY <93768814+yash2700@users.noreply.github.com>
2023-04-10 15:54:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your code here
+const images = document.querySelectorAll('.image');
+let draggedImage = null;
+
+// Add event listeners to all images to make them draggable
+images.forEach(image => {
+  image.addEventListener('dragstart', () => {
+    draggedImage = image;
+    image.classList.add('selected');
+  });
+
+  image.addEventListener('dragend', () => {
+    draggedImage = null;
+    image.classList.remove('selected');
+  });
+
+  image.addEventListener('dragover', e => {
+    e.preventDefault();
+  });
+
+  image.addEventListener('drop', () => {
+    if (draggedImage !== image) {
+      // Swap the background images of the dragged and dropped images
+      const temp = draggedImage.style.backgroundImage;
+      draggedImage.style.backgroundImage = image.style.backgroundImage;
+      image.style.backgroundImage = temp;
+    }
+  });
+});
